95f91fe76b69b1a5b16755c30af24100cd49a36b
Angelo Pirola <angelo9871@gmail.com>
2023-01-16 20:05:03 +0100

    Aggiunti converters

---
+++ Tool.DateTimeOnly/Converters/TimeOnlyConverter.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Tool.DateTimeOnly.Converters;
+
+public class TimeOnlyConverter : JsonConverter<TimeOnly>
+{
+    public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return TimeOnly.Parse(value!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value.ToString("HH:mm:ss.fff"));
+}
